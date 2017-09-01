755f62a6a532fe9887c69ad55df471f6b440e151
Jayleh <jaylehyun@gmail.com>
2017-08-31 22:30:58 -0700

    fixed traceback error, optimized writing to csv file

--- First_Web_Scrape.py
+++ First_Web_Scrape.py
@@ -1,7 +1,10 @@
 """
 This was my first web scraping project. I was scraping graphics cards information
 from newegg. I couldn't finish it because I was halted by a 'Robot' check, 
-<h1>Robot?</h1>. Was a sad day :'(
+<h1>Robot?</h1>.
+
+No recaptcha at the moment. Made a few changes; fixed the traceback AatributeError
+and wrote code so that commas were taken out of brand names, i.e. "PNY Tech, Inc"
 """
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
@@ -19,34 +22,25 @@ page_soup = soup(page_html, "html.parser")
 #grabs each product
 containers = page_soup.findAll("div", {"class":"item-container"})
 
-'''
-Here, I would have continue writing code to put the info in an csv file if it
-weren't for the 'Robot' check. Here is what I would write tho:
-
 with open("graphics_cards.csv", "w") as f:
 	headers = "brand, product_name, shipping\n"
 	f.write(headers)
-'''
 
-for container in containers:
-	brand = container.div.div.a.img["title"] #traceback here
-	'''brand_name = container.findAll("a", {"class":"item-brand"})
-	brand = brand_name'''
+	for container in containers:
+		# AttributeError of 'NoneType' here, fixed with try/except
+		try:
+			brand = container.div.div.a.img["title"]
+		except AttributeError:
+			pass
 	
-	title_container = container.findAll("a", {"class":"item-title"})
-	product_name = title_container[0].text
+		title_container = container.findAll("a", {"class":"item-title"})
+		product_name = title_container[0].text
 	
-	shipping_container = container.findAll("li", {"class":"price-ship"})
-	shipping = shipping_container[0].text.strip()
-	print("brand: " + brand)
-	print("product_name: " + product_name)
-	print("shipping: " + shipping)
+		shipping_container = container.findAll("li", {"class":"price-ship"})
+		shipping = shipping_container[0].text.strip()
+		
+		print("brand: " + str(brand))
+		print("product_name: " + product_name)
+		print("shipping: " + shipping)
 
-	#f.write(brand + "," + product_name.replace(",", "|") + shipping + "\n")
-
-'''
-I had trouble printing out the brand name. It printed them out, however, I had a
-traceback error. Either an attribute error of 'NoneType' or where index was out 
-of bounds. I learned I can fix the latter with try: (dosomething)/except: pass.
-'''
-print(page_soup.h1) #prints out <h1>Robot?</h1>
+		f.write(brand.replace(",", "") + "," + product_name.replace(",", "|") + "," + shipping + "\n")
