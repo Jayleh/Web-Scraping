6f8a4cf87332cb07a98b46a24b80c2f20858037c
Jayleh <jaylehyun@gmail.com>
2018-02-20 21:36:59 -0800

    linting code

--- First_Web_Scrape.py
+++ First_Web_Scrape.py
@@ -11,36 +11,36 @@ from bs4 import BeautifulSoup as soup
 
 my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
 
-#opening up connection, grabbing the page
+# opening up connection, grabbing the page
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 
-#html parsing
+# html parsing
 page_soup = soup(page_html, "html.parser")
 
-#grabs each product
-containers = page_soup.findAll("div", {"class":"item-container"})
+# grabs each product
+containers = page_soup.findAll("div", {"class": "item-container"})
 
 with open("graphics_cards.csv", "w") as f:
-	headers = "brand, product_name, shipping\n"
-	f.write(headers)
+    headers = "brand, product_name, shipping\n"
+    f.write(headers)
 
-	for container in containers:
-		# AttributeError of 'NoneType' here, fixed with try/except
-		try:
-			brand = container.div.div.a.img["title"]
-		except AttributeError:
-			pass
-	
-		title_container = container.findAll("a", {"class":"item-title"})
-		product_name = title_container[0].text
-	
-		shipping_container = container.findAll("li", {"class":"price-ship"})
-		shipping = shipping_container[0].text.strip()
-		
-		print("brand: " + str(brand))
-		print("product_name: " + product_name)
-		print("shipping: " + shipping)
+for container in containers:
+    # AttributeError of 'NoneType' here, fixed with try/except
+    try:
+        brand = container.div.div.a.img["title"]
+    except AttributeError:
+        pass
 
-		f.write(brand.replace(",", "") + "," + product_name.replace(",", "|") + "," + shipping + "\n")
+    title_container = container.findAll("a", {"class": "item-title"})
+    product_name = title_container[0].text
+
+    shipping_container = container.findAll("li", {"class": "price-ship"})
+    shipping = shipping_container[0].text.strip()
+
+    print("brand: " + str(brand))
+    print("product_name: " + product_name)
+    print("shipping: " + shipping)
+
+    f.write(brand.replace(",", "") + "," + product_name.replace(",", "|") + "," + shipping + "\n")
